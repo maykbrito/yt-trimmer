6e570cbd461ae9b806a877f2069c17a1cdeffbf8
maykbrito <maykbrito@gmail.com>
2020-11-22 02:04:20 -0300

    :sparkles: add utils

---
+++ robots/utils.js
@@ -0,0 +1,30 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+function toSeconds(start, end) {
+    const [startHour, startMinutes, startSeconds] = start.split(":");
+    const [startSec, startMiliseconds] = startSeconds.split(".");
+
+    const [endHour, endMinutes, endSeconds] = end.split(":");
+    const [endSec, endMiliseconds] = endSeconds.split(".");
+
+    const startToSeconds =
+        parseInt(startHour) * 60 + parseInt(startMinutes) * 60 + parseInt(startSec);
+
+    const endToSeconds =
+        parseInt(endHour) * 60 + parseInt(endMinutes) * 60 + parseInt(endSec);
+
+    const miliseconds = +(parseInt(endMiliseconds) - parseInt(startMiliseconds));
+
+    const toSeconds = +(parseInt(endToSeconds) - parseInt(startToSeconds));
+
+    console.log("===================================");
+    console.log(`start (${start}) - end (${end}): toSeconds: ${toSeconds}.${miliseconds}`);
+    return `${toSeconds}.${miliseconds}`;
+}
+
+
+module.exports = {
+    exec,
+    toSeconds,
+}
