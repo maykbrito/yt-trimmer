155675e2d7ab2ad6dbd80d28d0b1da46a5442444
Mayk Brito <maykbrito@gmail.com>
2021-08-16 20:19:16 -0300

    remove unused scripts dir

--- scripts/videourl.sh
+++
@@ -1,8 +0,0 @@
-# just to remember some scripts
-#youtubedl=./node_modules/youtube-dl/bin/youtube-dl
-
-# get video information
-#$youtubedl -F $1
-
-# get video url
-# $youtubedl --get-url -f best $1
